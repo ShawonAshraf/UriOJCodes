a2372f3712c2b1c0159419884da230c739c31a1a
Shawon Ashraf <Shawon Ashraf>
2016-10-12 21:19:59 +0600

    Fixed 1332

--- URI 1332/source/uri1332.py
+++ URI 1332/source/uri1332.py
@@ -1,6 +1,16 @@
 # uri 1332 solution
 
 
+def getErrorPercentage(text, ref_str):
+    counter = 0
+    for i in range(3):
+        if(ref_str[i] != text[i]):
+            counter = counter + 1
+
+    error_perc = counter / 3
+    return error_perc
+
+
 numDict = {"one": 1, "two": 2, "three": 3}
 
 testCases = int(input())
@@ -9,7 +19,7 @@ for i in range(testCases):
     l = len(num)
 
     if(l == 3):
-        if(num[0] == "o" or num[l - 1] == "e" or num[1] == "n"):
+        if(getErrorPercentage(num, "one") == (1 / 3)):
             print(numDict["one"])
         else:
             print(numDict["two"])
