3918c2a3ca9ab4798ba01b41ed30ea5a23e7d644
Shawon Ashraf <shawon13@live.com>
2016-10-14 23:17:36 +0600

    1161 Derped

--- URI 1161/uri1161.py
+++ URI 1161/uri1161.py
@@ -1,11 +1,22 @@
-def factorial(n):
-    if(n <= 1):
-        return 1
-    else:
-        return n * factorial(n - 1)
+factorials = []
+for i in range(21):
+    factorials.append(0)
 
-n = int(input())
-m = int(input())
+factorials[0] = 1
+factorials[1] = 1
 
-sum = factorial(n) + factorial(m)
-print(sum)
+for i in range(2, 20):
+    factorials[i] = factorials[i - 1] * i
+
+
+while(True):
+    line = input()
+    if line == "":
+        break
+    nums = line.split(" ")
+
+    a = int(nums[0])
+    b = int(nums[1])
+
+    sum = factorials[a] + factorials[b]
+    print(sum)
