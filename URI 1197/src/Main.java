203fafeeb14660007fd64338f095c2ea4a9cbd1c
Shawon Ashraf <shawon13@live.com>
2017-05-05 14:43:23 +0600

    Done 1197

--- URI 1197/src/Main.java
+++ URI 1197/src/Main.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNext()) {
+            int v = in.nextInt();
+            int t = in.nextInt();
+
+            int s = 2 * v * t;
+            System.out.println(s);
+        }
+
+        in.close();
     }
 }
