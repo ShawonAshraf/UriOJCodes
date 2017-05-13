b8eb01a589ebd782bdc039b361089f2d6888b199
Shawon Ashraf <shawon13@live.com>
2017-05-14 02:10:12 +0600

    partially works

--- URI 1262/src/Main.java
+++ URI 1262/src/Main.java
@@ -8,32 +8,26 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         while (in.hasNext()) {
-            String trace = in.nextLine();
+            String trace = in.nextLine().trim();
             int processes = in.nextInt();
             in.nextLine();
 
-            Stack<Character> cycleStack = new Stack<>();
             int l = trace.length();
             int cycleCount = 0;
+
+            int r_count = 0;
+            int w_count = 0;
 
             for (int i = 0; i < l; i++) {
                 char op = trace.charAt(i);
 
-                if (cycleStack.isEmpty() == false) {
-                    char top = cycleStack.peek();
-                    if (top == op) {
-                        cycleStack.push(op);
-                    } else {
-                        int n = cycleStack.size();
-                        cycleCount += (n / processes) + 1;
-                        cycleStack.clear();
-                    }
-
-                } else {
-                    cycleStack.push(op);
-                    cycleCount++;
-                }
+                if (op == 'R') r_count++;
+                else w_count++;
             }
+
+            double r_d = (double) r_count;
+            int r = (int)(Math.ceil(r_d % processes));
+            cycleCount = r + w_count;
 
             System.out.println(cycleCount);
         }
